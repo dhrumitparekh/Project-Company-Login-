16d6303c207094cae73caa8d778304273bbb622f
dhrumitparekh <137636355+dhrumitparekh@users.noreply.github.com>
2023-12-17 20:32:42 -0500

    Update server.js

--- server.js
+++ server.js
@@ -15,6 +15,26 @@ Data.get('/employeeData/Employees',(req,res)=>{
       })
     });
 
+Data.get('/employeeData/get_EmployeeNum',(req,res)=>{
+      const EmployeeNum = "3";
+EmployeeData.getEmployeeByNum(EmployeeNum).then((Employees)=>
+{
+  res.json(Employees);
+}).catch((error) => {
+  res.status(404).json({ error: error });
+});
+  });
+
+Data.get('/employeeData/get_EmployeeExp',(req,res)=>{
+      const EmployeeExp = "7";
+
+EmployeeData.getEmployeeByExp(EmployeeExp).then((Employees)=>
+{
+  res.json(Employees);
+}).catch((error) => {
+  res.status(404).json({ error: error });
+});
+  });
 
 Data.listen(port, () => {
     console.log('Server is listening on port ${port}');
