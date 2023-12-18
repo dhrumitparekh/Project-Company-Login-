d91c82e583d069e9364c22ae2d11a3b68818d047
dhrumitparekh <137636355+dhrumitparekh@users.noreply.github.com>
2023-12-17 20:36:29 -0500

    Update employeeData.js

--- modules/employeeData.js
+++ modules/employeeData.js
@@ -1,5 +1,6 @@
 const EmployeeData = require("../data/Employers.json");
 const jobData = require("../data/JobTitle.json");
+const DepartmentData = require("../Data/Department.json");
 let Employees =[];
 
 function Initialize(){
@@ -7,6 +8,7 @@ function Initialize(){
     {
     EmployeeData.forEach((Employeers)=>
     {
+        const DepID = Employeers.department;
         const EmpID = Employeers.job_title;
         const job = jobData.find((Title)=> Title.id === EmpID );
         if(job)
@@ -17,15 +19,53 @@ function Initialize(){
         {
            Employeers.job_title ="Not Defined";
         }
-
+        const dep= DepartmentData.find((name)=>name.id === DepID)
+        if (dep)
+        {
+          Employeers.department = dep.Department;
+        }
+        else
+        {
+         Employeers.department = "Not Valid";
+        }
     });
     Employees = EmployeeData;
     accepted();
     });
 }
+
 function getAllEmployees() {
     return new Promise((accepted) => {
       accepted(Employees);
     });
   }
-module.exports={Initialize,getAllEmployees};
+
+  function getEmployeeByNum(EmployeeNumber)
+  {
+    return new Promise((accepted,rejected) => {
+      const SelectedEmployee = Employees.find((EmployeeNum)=> EmployeeNum.id === EmployeeNumber)
+      if(!SelectedEmployee)
+      {
+        rejected("Unable to find the employee");
+      }
+      else{
+        accepted(SelectedEmployee);
+      }
+    });
+  }
+
+  function getEmployeeByExp(EmployeeExp)
+  {
+    return new Promise((accepted,rejected) => {
+      const SelectedEmployee = Employees.find((EmployeeNum)=> EmployeeNum.years_of_experience === EmployeeExp)
+      if(!SelectedEmployee)
+      {
+        rejected("Unable to find the employee");
+      }
+      else{
+        accepted(SelectedEmployee);
+      }
+    });
+  }
+  
+module.exports={Initialize,getAllEmployees,getEmployeeByNum,getEmployeeByExp};
