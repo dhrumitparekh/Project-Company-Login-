d601275a554b5710cfbbab5829ff213be97e3af0
dhrumitparekh <137636355+dhrumitparekh@users.noreply.github.com>
2024-01-03 17:52:57 -0500

    Update server.js

--- server.js
+++ server.js
@@ -6,10 +6,53 @@ const EmployeeData = require("./modules/employeeData");
 Data.use(express.static('public')); 
 Data.set('view engine', 'ejs');
 Data.use(express.urlencoded({ extended: true }));
+const authData = require('./modules/auth-service');
+const bodyParser = require('body-parser');
+const clientSessions = require('client-sessions');
 
+Data.use(bodyParser())
 EmployeeData.Initialize()
-Data.get('/', (req, res) => {
-  res.render("home");
+.then(authData.initialize)
+.then(function(){
+    Data.listen(HTTP_PORT, function(){
+        console.log(`Data listening on:  ${HTTP_PORT}`);
+    });
+}).catch(function(err){
+    console.log(`unable to start server: ${err}`);
+});
+
+EmployeeData.Initialize();
+
+Data.use((req, res, next) => {
+  Data.locals.currentRoute = req.path;
+  next();
+});
+
+Data.use(
+  clientSessions({
+    cookieName: 'session',
+    secret: 'your-secret-key',
+    duration: 24 * 60 * 60 * 1000,
+    activeDuration: 5 * 60 * 1000,
+  })
+);
+
+Data.use((req, res, next) => {
+  res.locals.session = req.session;
+  next();
+});
+
+const ensureLogin = (req, res, next) => {
+  if (!req.session.user) {
+    res.redirect('/');
+  } else {
+    next();
+  }
+};
+
+Data.get('/logout', (req, res) => {
+  req.session.reset(); 
+  res.redirect('/'); 
 });
 
 Data.get('/about', (req, res) => {
@@ -57,7 +100,7 @@ catch(error)
 }
   });
 
-  Data.get("/employeeData/addEmployees", async (req, res) => {
+  Data.get("/employeeData/addEmployees",ensureLogin,async (req, res) => {
     try {
       const Job = await EmployeeData.getAllJobTitles();
       const Dep = await EmployeeData.getAllDepartments();
@@ -68,7 +111,7 @@ catch(error)
   });
   
 
-  Data.post('/employeeData/addEmployees', async (req, res) => {
+  Data.post('/employeeData/addEmployees',ensureLogin,async (req, res) => {
     try {
 
       await EmployeeData.addEmployee(req.body);
@@ -79,7 +122,7 @@ catch(error)
     }
   });
 
-  Data.get('/employeeData/deleteEmployee/:id', async (req, res) => {
+  Data.get('/employeeData/deleteEmployee/:id',ensureLogin, async (req, res) => {
     try {
       await EmployeeData.deleteEmployee(req.params.id);
       res.redirect('/employeeData/Employees');
@@ -88,7 +131,7 @@ catch(error)
     }
   });
   
-Data.get('/employeeData/editEmployees/:id', async (req, res) => {
+Data.get('/employeeData/editEmployees/:id',ensureLogin,async (req, res) => {
   try {
     const Emp = await EmployeeData.getEmployeeByNum(req.params.id);
     const Job = await EmployeeData.getAllJobTitles();
@@ -100,7 +143,7 @@ Data.get('/employeeData/editEmployees/:id', async (req, res) => {
   }
 });
 
-Data.post('/employeeData/editEmployees', async (req, res) => {
+Data.post('/employeeData/editEmployees',ensureLogin, async (req, res) => {
   try {
     const empId = req.body.id; 
     const updatedData = req.body; 
@@ -113,6 +156,55 @@ Data.post('/employeeData/editEmployees', async (req, res) => {
   }
 });
 
+Data.get('/register', (req, res) => {
+  const errorMessage = '';
+  res.render('register', {
+    errorMessage
+  })
+})
+
+Data.post('/register', async (req, res) => {
+  try {
+    let { userName, email, password1, password2 } = req.body;
+    console.log(userName, email, password1, password2)
+    if (password1 != password2) return res.status(400).render('register', { errorMessage: "Passwords do not match" });
+    await authData.registerUser(userName, email, password1, password2);
+    res.status(201).redirect('/');
+  } catch (error) {
+    res.status(500).render('500', { message: `I'm sorry, but we have encountered the following error: ${error}` })
+  }
+})
+
+Data.get('/', (req, res) => {
+  try {
+    let errorMessage = '';
+    res.render('home', { errorMessage });
+  } catch (error) {
+    res.status(500).render('500', { message: `I'm sorry, but we have encountered the following error: ${error}` })
+  }
+})
+
+Data.post('/', async (req, res) => {
+  try {
+    const { userName, password } = req.body;
+    const userAgent = req.get('User-Agent');
+    console.log(userName, password, userAgent);
+    let user = await authData.checkUser(userName, password, userAgent);
+    if (user == 404) return res.render('home', { errorMessage: "User not fount" })
+    if (user == 400) return res.render('home', { errorMessage: "Password not match" })
+    req.session.user = user;
+    res.redirect('/employeeData/Employees');
+  } catch (error) {
+    res.status(500).render('500', { message: `I'm sorry, but we have encountered the following error: ${error}` })
+  }
+})
+
+Data.get('/userHistory', ensureLogin, (req, res) => {
+  let user = req.session?.user;
+  res.render('userHistory', {
+    user
+  })
+})
 
 Data.get('/employeeData/Employees/years/:years_of_experience',async(req,res)=>{
       const EmployeeExp = req.params.years_of_experience;
@@ -133,9 +225,7 @@ Data.get('/employeeData/Employees/years/:years_of_experience',async(req,res)=>{
 
 
 
-  Data.listen(HTTP_PORT, () => console.log(`server listening on: ${HTTP_PORT}`));
-
-
+  
 
 
 
